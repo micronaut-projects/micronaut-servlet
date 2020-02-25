6ac4fcf97e0e80673026f38b20f8798f37c9690a
graemerocher <graeme.rocher@gmail.com>
2020-02-25 17:41:49 +0100

    Cleanup

--- http-server-tomcat/src/main/java/io/micronaut/servlet/tomcat/TomcatConfiguration.java
+++ http-server-tomcat/src/main/java/io/micronaut/servlet/tomcat/TomcatConfiguration.java
@@ -9,13 +9,25 @@ import javax.annotation.Nullable;
 import javax.servlet.MultipartConfigElement;
 import java.util.Optional;
 
+/**
+ * Extends {@link HttpServerConfiguration} and allows configuring Tomcat.
+ *
+ * @author graemerocher
+ * @since 1.0
+ */
 @ConfigurationProperties("tomcat")
 public class TomcatConfiguration extends HttpServerConfiguration {
 
     @ConfigurationBuilder
     protected final Connector tomcatConnector;
     private final MultipartConfiguration multipartConfiguration;
+    private String protocol;
 
+    /**
+     * Default constructor.
+     * @param multipartConfiguration The multipart config
+     * @param protocol The protocol to use
+     */
     public TomcatConfiguration(
             @Nullable MultipartConfiguration multipartConfiguration,
             @Property(name = HttpServerConfiguration.PREFIX + ".tomcat.protocol")
@@ -24,6 +36,20 @@ public class TomcatConfiguration extends HttpServerConfiguration {
         this.tomcatConnector = new Connector(
                 protocol != null ? protocol : "org.apache.coyote.http11.Http11NioProtocol"
         );
+    }
+
+    /**
+     * @return The protocol to use. Defaults to org.apache.coyote.http11.Http11NioProtocol.
+     */
+    public String getProtocol() {
+        return protocol;
+    }
+
+    /**
+     * @param protocol The protocol to use. Defaults to org.apache.coyote.http11.Http11NioProtocol.
+     */
+    public void setProtocol(@Nullable String protocol) {
+        this.protocol = protocol;
     }
 
     /**
