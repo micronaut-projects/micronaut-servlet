d8e95c390e2bc99c1df191a2c6ee51e2f8357478
graemerocher <graeme.rocher@gmail.com>
2020-06-03 10:47:21 +0200

    Update build / licenses

--- http-server-tomcat/src/main/java/io/micronaut/servlet/tomcat/graal/TomcatSubs.java
+++ http-server-tomcat/src/main/java/io/micronaut/servlet/tomcat/graal/TomcatSubs.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -38,6 +38,8 @@ import org.apache.tomcat.util.modeler.NoDescriptorRegistry;
 import org.apache.tomcat.util.modeler.Registry;
 
 import java.security.ProtectionDomain;
+
+//CHECKSTYLE:OFF
 
 /**
  * Type hints and constants.
@@ -192,3 +194,5 @@ final class Org_apache_catalina_loader_WebappClassLoaderBase {
         }
     }
 }
+
+//CHECKSTYLE:ON
