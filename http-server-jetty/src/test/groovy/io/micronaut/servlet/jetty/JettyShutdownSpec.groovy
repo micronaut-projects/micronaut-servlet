d543198c1575ce675556d1ec8ff2c952463406ef
jameskleeh <james.kleeh@gmail.com>
2021-04-16 09:36:21 -0400

    Add tests for all impls without mocks

---
+++ http-server-jetty/src/test/groovy/io/micronaut/servlet/jetty/JettyShutdownSpec.groovy
@@ -0,0 +1,36 @@
+package io.micronaut.servlet.jetty
+
+import io.micronaut.context.ApplicationContext
+import io.micronaut.context.event.ApplicationEventListener
+import io.micronaut.runtime.server.EmbeddedServer
+import io.micronaut.runtime.server.event.ServerShutdownEvent
+import spock.lang.Specification
+
+import javax.inject.Singleton
+import java.util.concurrent.atomic.AtomicInteger
+
+class JettyShutdownSpec extends Specification {
+
+    void "test the shutdown event is emitted exactly once"() {
+        given:
+        EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer)
+        ShutdownListener listener = embeddedServer.applicationContext.getBean(ShutdownListener)
+
+        when:
+        embeddedServer.stop()
+
+        then:
+        listener.count.get() == 1
+    }
+
+    @Singleton
+    static class ShutdownListener implements ApplicationEventListener<ServerShutdownEvent> {
+
+        AtomicInteger count = new AtomicInteger()
+
+        @Override
+        void onApplicationEvent(ServerShutdownEvent event) {
+            count.incrementAndGet()
+        }
+    }
+}
