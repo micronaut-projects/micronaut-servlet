79c00bcad6fddfa881891ac1afabb81fcf471035
Sergio del Amo <sergio.delamo@softamo.com>
2021-04-21 13:22:41 +0200

    test: avoid bean pollution

--- http-server-tomcat/src/test/groovy/io/micronaut/servlet/tomcat/TomcatShutdownSpec.groovy
+++ http-server-tomcat/src/test/groovy/io/micronaut/servlet/tomcat/TomcatShutdownSpec.groovy
@@ -1,6 +1,7 @@
 package io.micronaut.servlet.tomcat
 
 import io.micronaut.context.ApplicationContext
+import io.micronaut.context.annotation.Requires
 import io.micronaut.context.event.ApplicationEventListener
 import io.micronaut.runtime.server.EmbeddedServer
 import io.micronaut.runtime.server.event.ServerShutdownEvent
@@ -13,7 +14,7 @@ class TomcatShutdownSpec extends Specification {
 
     void "test the shutdown event is emitted exactly once"() {
         given:
-        EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer)
+        EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer, ['spec.name': 'TomcatShutdownSpec'])
         ShutdownListener listener = embeddedServer.applicationContext.getBean(ShutdownListener)
 
         when:
@@ -23,6 +24,7 @@ class TomcatShutdownSpec extends Specification {
         listener.count.get() == 1
     }
 
+    @Requires(property = 'spec.name', value = 'TomcatShutdownSpec')
     @Singleton
     static class ShutdownListener implements ApplicationEventListener<ServerShutdownEvent> {
 
