cc7745e2173d0cc6b38f6b591a66e93e33441804
Sergio del Amo <sergio.delamo@softamo.com>
2023-01-10 09:31:46 +0100

    exclude RemoteAddressTest
    test is failing with: org.opentest4j.AssertionFailedError: expected: <127.0.0.1> but was: <10.1.0.227>

--- test-suite-http-server-tck-jetty/src/test/java/io/micronaut/http/server/tck/jetty/tests/JettytHttpServerTestSuite.java
+++ test-suite-http-server-tck-jetty/src/test/java/io/micronaut/http/server/tck/jetty/tests/JettytHttpServerTestSuite.java
@@ -8,6 +8,6 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 @Suite
 @SelectPackages("io.micronaut.http.server.tck.tests")
 @SuiteDisplayName("HTTP Server TCK for Jetty")
-@ExcludeClassNamePatterns(value = "io.micronaut.http.server.tck.tests.cors.CorsSimpleRequestTest|io.micronaut.http.server.tck.tests.MiscTest|io.micronaut.http.server.tck.tests.ErrorHandlerTest")
+@ExcludeClassNamePatterns(value = "io.micronaut.http.server.tck.tests.cors.CorsSimpleRequestTest|io.micronaut.http.server.tck.tests.MiscTest|io.micronaut.http.server.tck.tests.ErrorHandlerTest|io.micronaut.http.server.tck.tests.RemoteAddressTest")
 public class JettytHttpServerTestSuite {
 }
