ea55d25a0e8d3b39ccae33ec945e951e275d024c
Sergio del Amo <sergio.delamo@softamo.com>
2023-01-10 07:08:08 +0100

    MN: Update to 3.8.0 (#371)
    Backports fixes added in https://github.com/micronaut-projects/micronaut-servlet/pull/364

--- http-server-jetty/src/test/groovy/io/micronaut/servlet/jetty/JettySslSpec.groovy
+++ http-server-jetty/src/test/groovy/io/micronaut/servlet/jetty/JettySslSpec.groovy
@@ -1,6 +1,6 @@
 package io.micronaut.servlet.jetty
 
-import io.micronaut.context.annotation.Property
+import io.micronaut.context.ApplicationContext
 import io.micronaut.context.annotation.Requires
 import io.micronaut.core.util.StringUtils
 import io.micronaut.http.HttpRequest
@@ -8,50 +8,72 @@ import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.client.HttpClient
-import io.micronaut.http.client.annotation.Client
-import io.micronaut.test.extensions.spock.annotation.MicronautTest
-import io.micronaut.test.support.TestPropertyProvider
+import io.micronaut.runtime.server.EmbeddedServer
+import io.netty.handler.ssl.util.SelfSignedCertificate
 import spock.lang.Specification
-import jakarta.inject.Inject
 
-@Property(name = 'spec.name', value = 'JettySslSpec')
-@MicronautTest
-class JettySslSpec extends Specification implements TestPropertyProvider {
+import java.nio.file.Files
+import java.nio.file.Path
+import java.security.KeyStore
+import java.security.cert.Certificate
 
-    @Inject
-    @Client("/")
-    HttpClient rxClient
+class JettySslSpec extends Specification {
 
     void "test certificate extraction"() {
+        given:
+        Path keyStorePath = Files.createTempFile("micronaut-test-key-store", "pkcs12")
+        Path trustStorePath = Files.createTempFile("micronaut-test-trust-store", "jks")
+
+        def certificate = new SelfSignedCertificate()
+
+        KeyStore ks = KeyStore.getInstance("PKCS12")
+        ks.load(null, null)
+        ks.setKeyEntry("key", certificate.key(), "".toCharArray(), new Certificate[]{certificate.cert()})
+        try (OutputStream os = Files.newOutputStream(keyStorePath)) {
+            ks.store(os, "".toCharArray())
+        }
+
+        KeyStore ts = KeyStore.getInstance("JKS")
+        ts.load(null, null)
+        ts.setCertificateEntry("cert", certificate.cert())
+        try (OutputStream os = Files.newOutputStream(trustStorePath)) {
+            ts.store(os, "123456".toCharArray())
+        }
+
+        and:
+        EmbeddedServer server = ApplicationContext.run(EmbeddedServer, [
+                'spec.name'                                : 'JettySslSpec',
+                "micronaut.server.jetty.ssl.sni-host-check": StringUtils.FALSE,
+                'micronaut.ssl.enabled'                    : StringUtils.TRUE,
+                // Cannot be true!
+                'micronaut.server.ssl.build-self-signed'   : false,
+                'micronaut.ssl.clientAuthentication'       : "need",
+                'micronaut.ssl.key-store.path'             : "file://${keyStorePath.toString()}",
+                'micronaut.ssl.key-store.type'             : 'PKCS12',
+                'micronaut.ssl.key-store.password'         : '',
+                'micronaut.ssl.trust-store.path'           : "file://${trustStorePath.toString()}",
+                'micronaut.ssl.trust-store.type'           : 'JKS',
+                'micronaut.ssl.trust-store.password'       : '123456',
+
+        ])
+        HttpClient rxClient = server.getApplicationContext().createBean(HttpClient, URI.create("https://localhost:$server.port/"))
+
         when:
         def response = rxClient.toBlocking()
                 .exchange('/ssl', String)
-                
+
         then:
         response.code() == HttpStatus.OK.code
         response.body() == "true"
+
+        cleanup:
+        server.stop()
+        Files.deleteIfExists(keyStorePath)
+        Files.deleteIfExists(trustStorePath)
     }
 
-
-    @Override
-    Map<String, String> getProperties() {
-        [
-                "micronaut.server.jetty.ssl.sni-host-check": StringUtils.FALSE,
-                'micronaut.ssl.enabled': StringUtils.TRUE,
-                // Cannot be true!
-                'micronaut.ssl.buildSelfSigned': StringUtils.FALSE,
-                'micronaut.ssl.clientAuthentication': "need",
-                'micronaut.ssl.key-store.path': 'classpath:KeyStore.pkcs12',
-                'micronaut.ssl.key-store.type': 'PKCS12',
-                'micronaut.ssl.key-store.password': '',
-                'micronaut.ssl.trust-store.path': 'classpath:TrustStore.jks',
-                'micronaut.ssl.trust-store.type': 'JKS',
-                'micronaut.ssl.trust-store.password': '123456',
-        ]
-    }
-
-    @Requires(property = 'spec.name', value = 'JettySslSpec')
     @Controller
+    @Requires(property = 'spec.name', value = 'JettySslSpec')
     static class TestController {
 
         @Get('/ssl')
