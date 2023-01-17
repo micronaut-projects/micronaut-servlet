d025442ab34744e14ac0b9e5a693755328cea9d8
Tim Yates <tim.yates@gmail.com>
2023-01-17 13:26:35 +0000

    Bind to localhost instead of 0.0.0.0
    Previously, the servlet container was bound to all interfaces on 0.0.0.0
    
    This change switches to bind to localhost by default as we dso with the Netty server
    
    As it's a change in functionality, it's a change for 4.0.0 to be included in Micronaut 4

--- test-suite-http-server-tck-tomcat/src/test/java/io/micronaut/http/server/tck/tomcat/tests/TomcatHttpServerTestSuite.java
+++ test-suite-http-server-tck-tomcat/src/test/java/io/micronaut/http/server/tck/tomcat/tests/TomcatHttpServerTestSuite.java
@@ -8,6 +8,6 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 @Suite
 @SelectPackages("io.micronaut.http.server.tck.tests")
 @SuiteDisplayName("HTTP Server TCK for Tomcat")
-@ExcludeClassNamePatterns(value = "io.micronaut.http.server.tck.tests.FluxTest|io.micronaut.http.server.tck.tests.BodyTest|io.micronaut.http.server.tck.tests.cors.CorsSimpleRequestTest|io.micronaut.http.server.tck.tests.MiscTest|io.micronaut.http.server.tck.tests.ErrorHandlerTest|io.micronaut.http.server.tck.tests.RemoteAddressTest")
+@ExcludeClassNamePatterns(value = "io.micronaut.http.server.tck.tests.FluxTest|io.micronaut.http.server.tck.tests.BodyTest|io.micronaut.http.server.tck.tests.MiscTest|io.micronaut.http.server.tck.tests.ErrorHandlerTest")
 public class TomcatHttpServerTestSuite {
 }
--- test-suite-http-server-tck-undertow/src/test/java/io/micronaut/http/server/tck/undertow/tests/UndertowHttpServerTestSuite.java
+++ test-suite-http-server-tck-undertow/src/test/java/io/micronaut/http/server/tck/undertow/tests/UndertowHttpServerTestSuite.java
@@ -8,6 +8,6 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 @Suite
 @SelectPackages("io.micronaut.http.server.tck.tests")
 @SuiteDisplayName("HTTP Server TCK for Undertow")
-@ExcludeClassNamePatterns(value = "io.micronaut.http.server.tck.tests.RemoteAddressTest|io.micronaut.http.server.tck.tests.BodyTest|io.micronaut.http.server.tck.tests.cors.CorsSimpleRequestTest|io.micronaut.http.server.tck.tests.MiscTest|io.micronaut.http.server.tck.tests.ErrorHandlerTest")
+@ExcludeClassNamePatterns(value = "io.micronaut.http.server.tck.tests.BodyTest|io.micronaut.http.server.tck.tests.MiscTest|io.micronaut.http.server.tck.tests.ErrorHandlerTest")
 public class UndertowHttpServerTestSuite {
 }
