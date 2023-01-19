09a6f6203502166af43a6b0b92a6454a1b07b560
Tim Yates <tim.yates@gmail.com>
2023-01-19 11:38:13 +0000

    Merge 3.4.x into master (#379)

---
+++ test-suite-http-server-tck-tomcat/src/test/java/io/micronaut/http/server/tck/tomcat/tests/TomcatHttpServerTestSuite.java
@@ -0,0 +1,13 @@
+package io.micronaut.http.server.tck.tomcat.tests;
+
+import org.junit.platform.suite.api.ExcludeClassNamePatterns;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+@Suite
+@SelectPackages("io.micronaut.http.server.tck.tests")
+@SuiteDisplayName("HTTP Server TCK for Tomcat")
+@ExcludeClassNamePatterns(value = "io.micronaut.http.server.tck.tests.FluxTest|io.micronaut.http.server.tck.tests.BodyTest|io.micronaut.http.server.tck.tests.cors.CorsSimpleRequestTest|io.micronaut.http.server.tck.tests.MiscTest|io.micronaut.http.server.tck.tests.ErrorHandlerTest|io.micronaut.http.server.tck.tests.RemoteAddressTest")
+public class TomcatHttpServerTestSuite {
+}
