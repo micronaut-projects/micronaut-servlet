e1a650585bdd83b7cf8b28c012441288ad609ffb
Tim Yates <tim.yates@gmail.com>
2023-04-21 17:20:29 +0100

    Get the TCK tests to pass
    Since we've updated to latest versions, we should get more TCK tests passing.

--- test-suite-http-server-tck-jetty/src/test/java/io/micronaut/http/server/tck/jetty/tests/JettyHttpServerTestSuite.java
+++ test-suite-http-server-tck-jetty/src/test/java/io/micronaut/http/server/tck/jetty/tests/JettyHttpServerTestSuite.java
@@ -1,6 +1,5 @@
 package io.micronaut.http.server.tck.jetty.tests;
 
-import org.junit.platform.suite.api.ExcludeClassNamePatterns;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
@@ -8,12 +7,5 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 @Suite
 @SelectPackages("io.micronaut.http.server.tck.tests")
 @SuiteDisplayName("HTTP Server TCK for Jetty")
-@ExcludeClassNamePatterns({
-    "io.micronaut.http.server.tck.tests.cors.CorsSimpleRequestTest",
-    "io.micronaut.http.server.tck.tests.staticresources.StaticResourceTest", // fails on GraalVm
-    "io.micronaut.http.server.tck.tests.filter.ClientResponseFilterTest", // fails on GraalVM
-    "io.micronaut.http.server.tck.tests.ErrorHandlerTest" // fails on GraalVM
-
-})
 public class JettyHttpServerTestSuite {
 }
--- test-suite-http-server-tck-undertow/src/test/java/io/micronaut/http/server/tck/undertow/tests/UndertowHttpServerTestSuite.java
+++ test-suite-http-server-tck-undertow/src/test/java/io/micronaut/http/server/tck/undertow/tests/UndertowHttpServerTestSuite.java
@@ -9,11 +9,7 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 @SelectPackages("io.micronaut.http.server.tck.tests")
 @SuiteDisplayName("HTTP Server TCK for Undertow")
 @ExcludeClassNamePatterns({
-    "io.micronaut.http.server.tck.tests.cors.CorsSimpleRequestTest",
-    "io.micronaut.http.server.tck.tests.RemoteAddressTest",
-    "io.micronaut.http.server.tck.tests.staticresources.StaticResourceTest", // fails on GraalVm
-    "io.micronaut.http.server.tck.tests.filter.ClientResponseFilterTest", // fails on GraalVM
-    "io.micronaut.http.server.tck.tests.ErrorHandlerTest" // fails on GraalVM
+    "io.micronaut.http.server.tck.tests.RemoteAddressTest", // Undertow.getHost() reports an ipv6 address, not 127.0.0.1
 })
 public class UndertowHttpServerTestSuite {
 }
