96a022ba0bebc0dad854fa9a789fbd1f2876af0f
Tim Yates <tim.yates@gmail.com>
2023-08-16 12:52:39 +0100

    Using a proxy client in a filter fails under servlet
    We have a PR to add the TCK test here https://github.com/micronaut-projects/micronaut-core/pull/9724
    
    Working on a fix...

--- test-suite-http-server-tck-jetty/src/test/java/io/micronaut/http/server/tck/jetty/tests/JettyHttpServerTestSuite.java
+++ test-suite-http-server-tck-jetty/src/test/java/io/micronaut/http/server/tck/jetty/tests/JettyHttpServerTestSuite.java
@@ -1,6 +1,7 @@
 package io.micronaut.http.server.tck.jetty.tests;
 
 import org.junit.platform.suite.api.ExcludeClassNamePatterns;
+import org.junit.platform.suite.api.IncludeClassNamePatterns;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
@@ -14,7 +15,6 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 @ExcludeClassNamePatterns({
     "io.micronaut.http.server.tck.tests.staticresources.StaticResourceTest", // Graal fails to see /assets from the TCK as a resource https://ge.micronaut.io/s/ufuhtbe5sgmxi
     "io.micronaut.http.server.tck.tests.filter.ClientResponseFilterTest", // responseFilterThrowableParameter fails under Graal https://ge.micronaut.io/s/ufuhtbe5sgmxi
-    "io.micronaut.http.server.tck.tests.codec.JsonCodeAdditionalTypeTest", // remove once this pr is merged https://github.com/micronaut-projects/micronaut-core/pull/9419
 })
 public class JettyHttpServerTestSuite {
 }
