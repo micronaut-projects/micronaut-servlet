8f53293295d963b3a344c98f9a4f0f0ae9fc846f
Sergio del Amo <sergio.delamo@softamo.com>
2024-01-12 12:19:10 +0100

    build: remove dependency to http-netty

--- servlet-core/build.gradle.kts
+++ servlet-core/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
 dependencies {
     api(mn.micronaut.http.server)
     compileOnly(mn.micronaut.json.core)
-    implementation(mn.micronaut.http.netty)
     implementation(mn.micronaut.router)
     implementation(mnReactor.micronaut.reactor)
     testAnnotationProcessor(mn.micronaut.inject.java)
