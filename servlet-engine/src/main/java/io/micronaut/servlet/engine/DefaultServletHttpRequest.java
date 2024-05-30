4ef5bf35a8da5673b8523be818ed5e9cd37d0253
yawkat <jonas.konrad@oracle.com>
2024-05-30 17:58:03 +0200

    Implement byteBody

--- servlet-engine/src/main/java/io/micronaut/servlet/engine/DefaultServletHttpRequest.java
+++ servlet-engine/src/main/java/io/micronaut/servlet/engine/DefaultServletHttpRequest.java
@@ -37,8 +37,14 @@ import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpVersion;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.MutableHttpRequest;
+import io.micronaut.http.ServerHttpRequest;
+import io.micronaut.http.body.AvailableByteBody;
+import io.micronaut.http.body.ByteBody;
+import io.micronaut.http.body.CloseableAvailableByteBody;
+import io.micronaut.http.body.CloseableByteBody;
 import io.micronaut.http.codec.MediaTypeCodecRegistry;
 import io.micronaut.http.cookie.Cookies;
+import io.micronaut.servlet.engine.body.AvailableByteArrayBody;
 import io.micronaut.servlet.http.BodyBuilder;
 import io.micronaut.servlet.http.ByteArrayByteBuffer;
 import io.micronaut.servlet.http.ParsedBodyHolder;
@@ -54,6 +60,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InterruptedIOException;
+import java.io.UncheckedIOException;
 import java.net.InetSocketAddress;
 import java.net.URI;
 import java.nio.charset.Charset;
@@ -68,8 +76,14 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.OptionalLong;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Supplier;
+
+import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -88,7 +102,7 @@ public final class DefaultServletHttpRequest<B> implements
     ServletHttpRequest<HttpServletRequest, B>,
     ServletExchange<HttpServletRequest, HttpServletResponse>,
     StreamedServletMessage<B, byte[]>,
-    FullHttpRequest<B>,
+    ServerHttpRequest<B>,
     ParsedBodyHolder<B> {
 
     private static final Logger LOG = LoggerFactory.getLogger(DefaultServletHttpRequest.class);
@@ -103,11 +117,11 @@ public final class DefaultServletHttpRequest<B> implements
     private final DefaultServletHttpResponse<B> response;
     private final MediaTypeCodecRegistry codecRegistry;
     private final MutableConvertibleValues<Object> attributes;
+    private final MockStreamingBody byteBody = new MockStreamingBody();
     private DefaultServletCookies cookies;
     private Supplier<Optional<B>> body;
 
     private boolean bodyIsReadAsync;
-    private ByteArrayByteBuffer<B> servletByteBuffer;
     private B parsedBody;
 
     /**
@@ -323,7 +337,18 @@ public final class DefaultServletHttpRequest<B> implements
 
     @Override
     public InputStream getInputStream() throws IOException {
-        return servletByteBuffer != null ? servletByteBuffer.toInputStream() : delegate.getInputStream();
+        CompletableFuture<? extends CloseableAvailableByteBody> buffered = byteBody.buffered.get();
+        if (buffered == null) {
+            return delegate.getInputStream();
+        } else {
+            try (CloseableAvailableByteBody split = buffered.get().split()) {
+                return split.toInputStream();
+            } catch (InterruptedException e) {
+                throw new InterruptedIOException();
+            } catch (ExecutionException e) {
+                throw new IOException(e);
+            }
+        }
     }
 
     @Override
@@ -489,35 +514,8 @@ public final class DefaultServletHttpRequest<B> implements
     }
 
     @Override
-    public boolean isFull() {
-        return !bodyIsReadAsync;
-    }
-
-    @Override
-    public ByteBuffer<?> contents() {
-        if (bodyIsReadAsync) {
-            if (LOG.isDebugEnabled()) {
-                LOG.debug("Body is read asynchronously, cannot get contents");
-            }
-            return null;
-        }
-        try {
-            if (servletByteBuffer == null) {
-                this.servletByteBuffer = new ByteArrayByteBuffer<>(delegate.getInputStream().readAllBytes());
-            }
-            return servletByteBuffer;
-        } catch (IOException e) {
-            throw new IllegalStateException("Error getting all body contents", e);
-        }
-    }
-
-    @Override
-    public ExecutionFlow<ByteBuffer<?>> bufferContents() {
-        ByteBuffer<?> contents = contents();
-        if (contents == null) {
-            return null;
-        }
-        return ExecutionFlow.just(contents);
+    public @NonNull ByteBody byteBody() {
+        return byteBody;
     }
 
     /**
@@ -635,6 +633,64 @@ public final class DefaultServletHttpRequest<B> implements
                 }
             }
             return Optional.empty();
+        }
+    }
+
+    /**
+     * Temporary streaming {@link ByteBody} implementation that only supports buffering, for filter
+     * body binding to work. Will be replaced by a proper streaming implementation.
+     */
+    private class MockStreamingBody implements CloseableByteBody {
+        private final AtomicReference<CompletableFuture<? extends CloseableAvailableByteBody>> buffered = new AtomicReference<>();
+
+        @Override
+        public void close() {
+        }
+
+        @Override
+        public @NonNull CloseableByteBody split(SplitBackpressureMode backpressureMode) {
+            return this;
+        }
+
+        @Override
+        public @NonNull OptionalLong expectedLength() {
+            return OptionalLong.empty();
+        }
+
+        @Override
+        public @NonNull InputStream toInputStream() {
+            throw new UnsupportedOperationException("Streaming access not yet implemented for servlet");
+        }
+
+        @Override
+        public @NonNull Publisher<byte[]> toByteArrayPublisher() {
+            throw new UnsupportedOperationException("Streaming access not yet implemented for servlet");
+        }
+
+        @Override
+        public @NonNull Publisher<ByteBuffer<?>> toByteBufferPublisher() {
+            throw new UnsupportedOperationException("Streaming access not yet implemented for servlet");
+        }
+
+        @Override
+        public CompletableFuture<? extends CloseableAvailableByteBody> buffer() {
+            if (bodyIsReadAsync) {
+                throw new UnsupportedOperationException("Body is read asynchronously, cannot get contents");
+            }
+            CompletableFuture<CloseableAvailableByteBody> dest = new CompletableFuture<>();
+            CompletableFuture<? extends CloseableAvailableByteBody> result;
+            if (buffered.compareAndSet(null, dest)) {
+                try {
+                    dest.complete(new AvailableByteArrayBody(delegate.getInputStream().readAllBytes()));
+                } catch (Throwable t) {
+                    dest.completeExceptionally(t);
+                }
+                result = dest;
+            } else {
+                result = buffered.get();
+            }
+            // give each caller their own body
+            return result.thenApply(AvailableByteBody::split);
         }
     }
 }
---
+++ servlet-engine/src/main/java/io/micronaut/servlet/engine/body/AvailableByteArrayBody.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2017-2024 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.servlet.engine.body;
+
+import io.micronaut.core.annotation.Internal;
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.core.io.buffer.ByteBuffer;
+import io.micronaut.http.body.CloseableAvailableByteBody;
+import io.micronaut.servlet.http.ByteArrayByteBuffer;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * {@link io.micronaut.http.body.AvailableByteBody} implementation based on a byte array.
+ *
+ * @author Jonas Konrad
+ * @since 4.9.0
+ */
+@Internal
+public final class AvailableByteArrayBody extends AbstractServletByteBody implements CloseableAvailableByteBody {
+    private byte[] array;
+
+    public AvailableByteArrayBody(byte[] array) {
+        this.array = array;
+    }
+
+    @Override
+    public @NonNull CloseableAvailableByteBody split() {
+        if (array == null) {
+            failClaim();
+        }
+        return new AvailableByteArrayBody(array);
+    }
+
+    @Override
+    public @NonNull InputStream toInputStream() {
+        return new ByteArrayInputStream(array);
+    }
+
+    @Override
+    public CompletableFuture<? extends CloseableAvailableByteBody> buffer() {
+        if (array == null) {
+            failClaim();
+        }
+        CompletableFuture<AvailableByteArrayBody> f = CompletableFuture.completedFuture(new AvailableByteArrayBody(array));
+        array = null;
+        return f;
+    }
+
+    @Override
+    public long length() {
+        if (array == null) {
+            failClaim();
+        }
+        return array.length;
+    }
+
+    @Override
+    public byte @NonNull [] toByteArray() {
+        byte[] a = array;
+        if (a == null) {
+            failClaim();
+        }
+        array = null;
+        return a;
+    }
+
+    @Override
+    public @NonNull ByteBuffer<?> toByteBuffer() {
+        return new ByteArrayByteBuffer<>(toByteArray());
+    }
+
+    @Override
+    public void close() {
+        array = null;
+    }
+}
