3c6a464afc2dfcbdb472c684c1663b7c3fd6fd11
Andriy Dmytruk <andriy.dmytruk@oracle.com>
2024-06-28 10:22:24 -0400

    Spotless and binary compatability fixes

--- http-poja-test/src/main/java/io/micronaut/http/poja/test/TestingServerlessApplication.java
+++ http-poja-test/src/main/java/io/micronaut/http/poja/test/TestingServerlessApplication.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2024 Oracle and/or its affiliates.
+ * Copyright 2017-2024 original authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
