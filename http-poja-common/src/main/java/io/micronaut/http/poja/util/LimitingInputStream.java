5213a917c44f73693ea43db60ece518811ea597c
Andriy Dmytruk <andriy.dmytruk@oracle.com>
2024-08-16 11:14:49 -0400

    Fix the LimitingInputStream

--- http-poja-common/src/main/java/io/micronaut/http/poja/util/LimitingInputStream.java
+++ http-poja-common/src/main/java/io/micronaut/http/poja/util/LimitingInputStream.java
@@ -17,7 +17,6 @@ package io.micronaut.http.poja.util;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 
 /**
  * A wrapper around input stream that limits the maximum size to be read.
@@ -34,12 +33,11 @@ public class LimitingInputStream extends InputStream {
     }
 
     @Override
-    public synchronized void mark(int readlimit) {
-        stream.mark(readlimit);
-    }
-
-    @Override
     public int read() throws IOException {
+        if (size >= maxSize) {
+            return -1;
+        }
+        ++size;
         return stream.read();
     }
 
@@ -49,9 +47,12 @@ public class LimitingInputStream extends InputStream {
             if (size >= maxSize) {
                 return -1;
             }
+            if (b.length + size > maxSize) {
+                return read(b, 0, (int) (maxSize - size));
+            }
             int sizeRead = stream.read(b);
             size += sizeRead;
-            return size > maxSize ? sizeRead + (int) (maxSize - size) : sizeRead;
+            return sizeRead;
         }
     }
 
@@ -61,40 +62,16 @@ public class LimitingInputStream extends InputStream {
             if (size >= maxSize) {
                 return -1;
             }
-            int sizeRead = stream.read(b, off, len);
-            size += sizeRead + off;
-            return size > maxSize ? sizeRead + (int) (maxSize - size) : sizeRead;
+            int lengthToRead = (int) Math.min(len, maxSize - size);
+            int sizeRead = stream.read(b, off, lengthToRead);
+            size += sizeRead;
+            return sizeRead;
         }
     }
 
     @Override
-    public byte[] readAllBytes() throws IOException {
-        return stream.readNBytes((int) (maxSize - size));
-    }
-
-    @Override
-    public byte[] readNBytes(int len) throws IOException {
-        return stream.readNBytes(len);
-    }
-
-    @Override
-    public int readNBytes(byte[] b, int off, int len) throws IOException {
-        return stream.readNBytes(b, off, len);
-    }
-
-    @Override
-    public long skip(long n) throws IOException {
-        return stream.skip(n);
-    }
-
-    @Override
-    public void skipNBytes(long n) throws IOException {
-        stream.skipNBytes(n);
-    }
-
-    @Override
     public int available() throws IOException {
-        return stream.available();
+        return (int) (maxSize - size);
     }
 
     @Override
@@ -102,33 +79,4 @@ public class LimitingInputStream extends InputStream {
         stream.close();
     }
 
-    @Override
-    public synchronized void reset() throws IOException {
-        stream.reset();
-    }
-
-    @Override
-    public boolean markSupported() {
-        return stream.markSupported();
-    }
-
-    @Override
-    public long transferTo(OutputStream out) throws IOException {
-        return stream.transferTo(out);
-    }
-
-    @Override
-    public int hashCode() {
-        return stream.hashCode();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return stream.equals(obj);
-    }
-
-    @Override
-    public String toString() {
-        return stream.toString();
-    }
 }
