07cb14c0ee48f0e47d80f19bd948d0c1c9f559a4
Andriy Dmytruk <andriy.dmytruk@oracle.com>
2024-08-26 12:31:41 -0400

    Fix the flaky HTTP POJA test

--- http-poja-apache/src/test/groovy/io/micronaut/http/poja/SimpleServerSpec.groovy
+++ http-poja-apache/src/test/groovy/io/micronaut/http/poja/SimpleServerSpec.groovy
@@ -5,115 +5,122 @@ import io.micronaut.core.annotation.NonNull
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.*
+import io.micronaut.runtime.server.EmbeddedServer
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import jakarta.inject.Inject
+import jakarta.inject.Singleton
+import spock.lang.Specification
 import spock.lang.Stepwise
 
 @Stepwise
 @MicronautTest
-class SimpleServerSpec extends BaseServerlessApplicationSpec {
+class SimpleServerSpec extends Specification {
+
+    @Inject
+    StringTestingClient client
 
     void "test GET method"() {
         when:
-        app.write("""\
-        GET /test HTTP/1.1
-        Host: h
-
-        """.stripIndent())
+        var response = client.exchange(unindent("""
+        GET /test HTTP/1.1\r
+        Host: h\r
+        \r
+        """))
 
         then:
-        app.read() == """\
-        HTTP/1.1 200 Ok
-        Content-Length: 32
-        Content-Type: text/plain
-
+        response == unindent("""
+        HTTP/1.1 200 Ok\r
+        Content-Length: 32\r
+        Content-Type: text/plain\r
+        \r
         Hello, Micronaut Without Netty!
-        """.stripIndent()
+        """)
     }
 
     void "test invalid GET method"() {
         when:
-        app.write("""\
-        GET /invalid-test HTTP/1.1
-        Host: h
-
-        """.stripIndent())
+        var response = client.exchange(unindent("""
+        GET /invalid-test HTTP/1.1\r
+        Host: h\r
+        \r
+        """))
 
         then:
-        app.read() == """\
-        HTTP/1.1 404 Not Found
-        Content-Length: 140
-        Content-Type: application/json
-
-        {"_links":{"self":[{"href":"/invalid-test","templated":false}]},"_embedded":{"errors":[{"message":"Page Not Found"}]},"message":"Not Found"}""".stripIndent()
+        response == unindent("""\
+        HTTP/1.1 404 Not Found\r
+        Content-Length: 140\r
+        Content-Type: application/json\r
+        \r
+        {"_links":{"self":[{"href":"/invalid-test","templated":false}]},"_embedded":{"errors":[{"message":"Page Not Found"}]},"message":"Not Found"}""")
     }
 
     void "test non-parseable GET method"() {
         when:
-        app.write("""\
-        GET /test HTTP/1.1error
-        Host: h
-
-        """.stripIndent())
+        var response = client.exchange(unindent("""
+        GET /test HTTP/1.1error\r
+        Host: h\r
+        \r
+        """))
 
         then:
-        app.read() == """\
-        HTTP/1.1 400 Bad Request
-        Content-Length: 32
-        Content-Type: text/plain
-
-        HTTP request could not be parsed""".stripIndent()
+        response == unindent("""
+        HTTP/1.1 400 Bad Request\r
+        Content-Length: 32\r
+        Content-Type: text/plain\r
+        \r
+        HTTP request could not be parsed""")
     }
 
     void "test DELETE method"() {
         when:
-        app.write("""\
-        DELETE /test HTTP/1.1
-        Host: h
-
-        """.stripIndent())
+        var response = client.exchange(unindent("""
+        DELETE /test HTTP/1.1\r
+        Host: h\r
+        \r
+        """))
 
         then:
-        app.read() == """\
-        HTTP/1.1 200 Ok
-        Content-Length: 0
-
-        """.stripIndent()
+        response == unindent("""
+        HTTP/1.1 200 Ok\r
+        Content-Length: 0\r
+        \r
+        """)
     }
 
     void "test POST method"() {
         when:
-        app.write("""\
-        POST /test/Dream HTTP/1.1
-        Host: h
-
-        """.stripIndent())
+        var response = client.exchange(unindent("""
+        POST /test/Dream HTTP/1.1\r
+        Host: h\r
+        \r
+        """))
 
         then:
-        app.read() == """\
-        HTTP/1.1 201 Created
-        Content-Length: 13
-        Content-Type: text/plain
-
+        response == unindent("""
+        HTTP/1.1 201 Created\r
+        Content-Length: 13\r
+        Content-Type: text/plain\r
+        \r
         Hello, Dream
-        """.stripIndent()
+        """)
     }
 
     void "test PUT method"() {
         when:
-        app.write("""\
-        PUT /test/Dream1 HTTP/1.1
-        Host: h
-
-        """.stripIndent())
+        var response = client.exchange(unindent("""
+        PUT /test/Dream1 HTTP/1.1\r
+        Host: h\r
+        \r
+        """))
 
         then:
-        app.read() == """\
-        HTTP/1.1 200 Ok
-        Content-Length: 15
-        Content-Type: text/plain
-
+        response == unindent("""
+        HTTP/1.1 200 Ok\r
+        Content-Length: 15\r
+        Content-Type: text/plain\r
+        \r
         Hello, Dream1!
-        """.stripIndent()
+        """)
     }
 
     /**
@@ -142,6 +149,40 @@ class SimpleServerSpec extends BaseServerlessApplicationSpec {
         @Status(HttpStatus.OK)
         String update(@NonNull String name) {
             return "Hello, " + name + "!\n"
+        }
+
+    }
+
+    private String unindent(String value, int indentSpaces = 8) {
+        while (value.charAt(0) == '\n' as char) {
+            value = value.substring(1)
+        }
+        var lines = value.split("\n")
+            .collect({ it.startsWith(" ".repeat(indentSpaces)) ? it.substring(indentSpaces) : it })
+            .join("\n")
+    }
+
+    @Singleton
+    static class StringTestingClient {
+
+        private EmbeddedServer server
+
+        StringTestingClient(EmbeddedServer server) {
+            this.server = server
+        }
+
+        String exchange(String request) {
+            try (Socket socket = new Socket(server.host, server.port)) {
+                OutputStream output = socket.getOutputStream()
+                output.write(request.getBytes())
+
+                InputStream input = socket.getInputStream()
+                return new String(input.readAllBytes())
+                        .replaceAll("Date:[^\r]+\r\n", "")
+            } catch (IOException ex) {
+                System.out.println("Could not exchange request with server: " + ex.getMessage());
+                ex.printStackTrace();
+            }
         }
 
     }
