7e90235e450c3c3e050a22cf13a18203e341319e
Sergio del Amo <sergio.delamo@softamo.com>
2025-01-22 11:58:23 +0100

    add back null check

--- http-server-jetty/src/main/java/io/micronaut/servlet/jetty/JettyFactory.java
+++ http-server-jetty/src/main/java/io/micronaut/servlet/jetty/JettyFactory.java
@@ -374,24 +374,26 @@ public class JettyFactory extends ServletServerFactory {
     }
 
     private void applyAdditionalPorts(Server server, ServerConnector serverConnector) {
-        Set<Integer> exposedPorts = router.getExposedPorts();
-        if (CollectionUtils.isNotEmpty(exposedPorts)) {
-            for (Integer exposedPort : exposedPorts) {
-                if (!exposedPort.equals(serverConnector.getLocalPort())) {
-                    ServerConnector connector;
-                    if (exposedPort.equals(endpointDefaultConfiguration.getPort().orElse(-1))) {
-                        connector = new ServerConnector(
-                        server,
-                        serverConnector.getConnectionFactories().stream().filter(x -> !x.getProtocol().equals("SSL")).toArray(ConnectionFactory[]::new));
-                    } else {
-                         connector = new ServerConnector(
-                            server,
-                            serverConnector.getConnectionFactories().toArray(ConnectionFactory[]::new)
-                        );
+        if (router != null) {
+            Set<Integer> exposedPorts = router.getExposedPorts();
+            if (CollectionUtils.isNotEmpty(exposedPorts)) {
+                for (Integer exposedPort : exposedPorts) {
+                    if (!exposedPort.equals(serverConnector.getLocalPort())) {
+                        ServerConnector connector;
+                        if (exposedPort.equals(endpointDefaultConfiguration.getPort().orElse(-1))) {
+                            connector = new ServerConnector(
+                                server,
+                                serverConnector.getConnectionFactories().stream().filter(x -> !x.getProtocol().equals("SSL")).toArray(ConnectionFactory[]::new));
+                        } else {
+                            connector = new ServerConnector(
+                                server,
+                                serverConnector.getConnectionFactories().toArray(ConnectionFactory[]::new)
+                            );
+                        }
+                        connector.setPort(exposedPort);
+                        connector.setHost(getConfiguredHost());
+                        server.addConnector(connector);
                     }
-                    connector.setPort(exposedPort);
-                    connector.setHost(getConfiguredHost());
-                    server.addConnector(connector);
                 }
             }
         }
