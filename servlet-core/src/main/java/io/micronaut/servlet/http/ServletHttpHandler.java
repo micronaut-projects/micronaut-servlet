baf1d2cc23558de0e97d6e423739f3bd179c1b71
Sergio del Amo <sergio.delamo@softamo.com>
2025-12-05 11:38:46 +0100

    remove workaround for bug already fixed in Micronaut core

--- servlet-core/src/main/java/io/micronaut/servlet/http/ServletHttpHandler.java
+++ servlet-core/src/main/java/io/micronaut/servlet/http/ServletHttpHandler.java
@@ -101,10 +101,6 @@ public abstract class ServletHttpHandler<REQ, RES> implements AutoCloseable, Lif
         this.routeExecutor = applicationContext.getBean(RouteExecutor.class);
         this.conversionService = conversionService;
         this.ioExecutor = SupplierUtil.memoized(() -> applicationContext.getBean(Executor.class, Qualifiers.byName(TaskExecutors.BLOCKING)));
-
-        // hack for bug fixed in Micronaut 1.3.3
-        applicationContext.getEnvironment()
-            .addConverter(HttpRequest.class, HttpRequest.class, httpRequest -> httpRequest);
     }
 
     /**
